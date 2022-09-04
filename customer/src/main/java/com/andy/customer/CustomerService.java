06337899fcc8a06dc7abca16c0643bb463199318
Andy <andycostintoma@gmail.com>
2022-09-04 15:35:22 +0300

    end of section 7

--- customer/src/main/java/com/andy/customer/CustomerService.java
+++ customer/src/main/java/com/andy/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.andy.customer;
 
+import com.andy.amqp.RabbitMQMessageProducer;
 import com.andy.clients.fraud.FraudCheckResponse;
 import com.andy.clients.fraud.FraudClient;
 import com.andy.clients.notification.NotificationClient;
@@ -12,8 +13,7 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
-
-    private final NotificationClient notificationClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
 
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
@@ -33,14 +33,17 @@ public class CustomerService {
             throw new IllegalStateException("Fraudster!");
         }
 
-        // todo: send notification - make it async. i.e add to queue
-        notificationClient.sendNotification(
-                new NotificationRequest(
-                        customer.getId(),
-                        customer.getEmail(),
-                        String.format("Hi %s, welcome to Andy's microservices!",
-                                customer.getFirstName())
-                )
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome to Andy's microservices!",
+                        customer.getFirstName())
+        );
+
+        rabbitMQMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
         );
     }
 }
--- notification/src/main/java/com/andy/notification/NotificationApplication.java
+++ notification/src/main/java/com/andy/notification/NotificationApplication.java
@@ -1,13 +1,34 @@
 package com.andy.notification;
 
+import com.andy.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.andy.notification",
+                "com.andy.amqp"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class,args);
     }
+
+// just for testing
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish(new Person("Ali", 18),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
